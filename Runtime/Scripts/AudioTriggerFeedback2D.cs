be08236c255f79a1db0fbebc42897cce74165be4
Eric Sh <eric@shedokan.com>
2026-03-16 16:15:20 +0200

    fix: Separate 2D and 3D Physics triggers

---
+++ Runtime/Scripts/AudioTriggerFeedback2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JSAM;
+
+namespace JSAM 
+{
+    [AddComponentMenu("AudioManager/Audio Trigger Feedback 2D")]
+    public class AudioTriggerFeedback2D : BaseAudioTriggerFeedback
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (triggerEvent == TriggerEvent.OnTriggerEnter) TriggerSound(collision);
+        }
+        
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (triggerEvent == TriggerEvent.OnTriggerStay) TriggerSound(collision);
+        }
+        
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (triggerEvent == TriggerEvent.OnTriggerExit) TriggerSound(collision);
+        }
+    }
+}
---
+++ Runtime/Scripts/BaseAudioTriggerFeedback.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JSAM;
+
+namespace JSAM 
+{
+    public class BaseAudioTriggerFeedback : BaseAudioFeedback<SoundFileObject>
+    {
+        enum TriggerEvent
+        {
+            OnTriggerEnter,
+            OnTriggerStay,
+            OnTriggerExit
+        }
+
+        [Header("Trigger Settings")]
+        [SerializeField]
+        [Tooltip("Will only play sound on trigger with another object on these layers")]
+        LayerMask triggersWith = 0;
+
+        [SerializeField]
+        [Tooltip("The intersection event that triggers the sound to play")]
+        TriggerEvent triggerEvent = TriggerEvent.OnTriggerEnter;
+
+        void TriggerSound(Collider other)
+        {
+            if (triggersWith.Contains(other.gameObject.layer))
+            {
+                AudioManager.PlaySound(audio, transform);
+            }
+        }
+
+        void TriggerSound(Collider2D collision)
+        {
+            if (triggersWith.Contains(collision.gameObject.layer))
+            {
+                AudioManager.PlaySound(audio, transform);
+            }
+        }
+    }
+}
